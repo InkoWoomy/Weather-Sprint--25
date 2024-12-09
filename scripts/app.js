d9c9a96eaedd6b9d453a0efc7424dcbc9f44f7c8
Alexander <yoda4115@gmail.com>
2024-12-09 14:53:18 -0800

    Images and data population

---
+++ scripts/app.js
@@ -0,0 +1,44 @@
+import { APIKEY } from "./enviroment.js"; 
+
+//This is a built-in API function that allows us to get the current location of the user
+
+//navigator returns the geolocation object
+//getCurrentPosition() returns the current position of the user
+
+navigator.geolocation.getCurrentPosition( success  );
+
+// (success , errorFunc)
+// Think of this as an if/else statement. If the user asppects it is successful, if not it's an error.
+
+//Example of a geolocation object
+{
+    coords: {
+        latitude: 37.7749;
+        longitude: -122.4194;
+    }
+}
+
+function success(position)
+{
+    console.log(position);
+    console.log("Latitude: " + position.coords.latitude);
+    console.log("Longitude: " + position.coords.longitude);
+}
+
+success();
+
+// function errorFunc (error){
+//     console.log(error.message);
+// }
+
+function apiCall() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=-121.2908&appid=${APIKEY}`)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data);
+    })
+}
+
+apiCall()
