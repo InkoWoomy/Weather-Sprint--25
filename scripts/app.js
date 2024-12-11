c92b759acf19beed7665b5c77efe9feb47784266
Alexander <yoda4115@gmail.com>
2024-12-11 15:25:54 -0800

    Data population and beginning data implementation

--- scripts/app.js
+++ scripts/app.js
@@ -1,48 +1,71 @@
+//Importing the API key from enviroment.js, so we can have enviroment.js (and the API key as a result) ignored by git.
 import { APIKEY } from './enviroment.js';
 
+//Call variables for data implementation
+const city = document.getElementById("city");
+const searchButton = document.getElementById("searchButton");
+const userInputCity = document.getElementById("userInputCity");
 
-//This is a built-in API function that allows us to get the current location of the user
+
+//Obtain Weather data from API
+
+//Test Data 
+let searchedCity = "";
+let lat = 0;
+let lon = 0;
 
 
-//navigator returns the geolocation object
-//getCurrentPosition() returns the current position of the user
-
-navigator.geolocation.getCurrentPosition( success , errorFunc );
-// `http://api.openweathermap.org/data/2.5/forecast?q=${currentCity},${currentCountry}&appid=${apiKey}`
-
-// (success , errorFunc)
-// Think of this as an if/else statement. If the user asppects it is successful, if not it's an error.
-
-//Example of a geolocation object
-// {
-//     coords: {
-//         latitude: 37.7749;
-//         longitude: -122.4194;
-//     }
-// }
-
-function success(position)
+//getLocation will populate data for a specific location when the user searches for it
+function getLocation()
 {
-    console.log(position);
-    console.log("Our latitude: " + position.coords.latitude);
-    console.log("Our longitude: " + position.coords.longitude);
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchedCity}&limit=1&appid=${APIKEY}`)
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            console.log(data);
+            lat = (data[0].lat);
+            lon = (data[0].lon);
+            console.log(`Latitude: ${lat}`);
+            console.log(`Longitude ${lon}`);
+        })
 }
 
-success();
-
-
-function errorFunc (error){ 
-    console.log(error.message);
-} 
-
-function apiCall() {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=-121.2908&appid=${APIKEY}`)
+function getCurrentDay(){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=imperial`)
     .then((response) => {
         return response.json()
     })
-    .then((data) => {
-        console.log(data);
-    })
+    .then((currentDay) => {
+        console.log(currentDay);
+    });
 }
 
-apiCall()
+function get5Day(){
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=imperial`)
+    .then((response) => {
+        return response.json()
+    })
+    .then((fiveDay) => {
+        console.log(fiveDay);
+    });
+}
+
+
+//getWeather will get data from getLocation after the user searches, so that we can find the weather data.
+function getWeather(){
+    getLocation(searchedCity);
+    getCurrentDay();
+    get5Day();
+    
+}
+
+
+
+searchButton.addEventListener('click', function()
+{
+    searchedCity = userInputCity.value.toLowerCase();
+    getWeather();
+    city.innerText = searchedCity;
+})
+
